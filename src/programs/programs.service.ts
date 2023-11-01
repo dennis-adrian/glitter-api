440ca5a9499beceeaa817130d25432972ed51df4
dennis-adrian <dennisguzmanbo@gmail.com>
2023-10-31 22:41:01 -0400

    Add some exceptions usage

--- src/programs/programs.service.ts
+++ src/programs/programs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ProgramsList } from './utils/programsList';
 import { Program } from './entities/program.entity';
 import { CreateProgramDto } from './dto/create-program.dto';
@@ -12,7 +12,12 @@ export class ProgramsService {
   }
 
   findOne(id: number): Program {
-    return this.programs.find((program) => program.id === id);
+    const program = this.programs.find((program) => program.id === id);
+    if (!program) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return program;
   }
 
   create(payload: CreateProgramDto): Program {
